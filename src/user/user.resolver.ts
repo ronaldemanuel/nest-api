f4f0371a21b92c2d48c5708693b60e1d2d233eeb
ronaldemanuel <emronald15el@gmail.com>
2021-09-11 18:26:27 -0300

    feat: implement user resolver with user mutation function

---
+++ src/user/user.resolver.ts
@@ -0,0 +1,18 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+
+@Resolver()
+export class UserResolver {
+  constructor(private userService: UserService) {
+    return;
+  }
+
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserInput): Promise<User> {
+    const user = await this.userService.createUser(data);
+    return user;
+  }
+}
