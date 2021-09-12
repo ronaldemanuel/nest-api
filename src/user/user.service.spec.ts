1fc5f8a07ef61054ea0f0bbfa3a16aa6dad0ca00
ronaldemanuel <emronald15el@gmail.com>
2021-09-12 18:18:47 -0300

    feat: define mock repository specs

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
