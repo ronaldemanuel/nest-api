c9eb2ea894505ff91210c6532fef0837978ae0a0
ronaldemanuel <emronald15el@gmail.com>
2021-09-13 22:37:28 -0300

    feat: import and config jwt module on auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,20 @@ import { AuthResolver } from './auth.resolver';
 import { UserService } from 'src/user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: '30s',
+        },
+      }),
+    }),
+  ],
   providers: [AuthService, AuthResolver, UserService],
 })
 export class AuthModule {
